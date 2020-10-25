bfe7739f642bdce20c3d44210c11bd7af51336eb
admin-45053 <admin-45053@admin-45053-PC>
2020-10-25 12:57:08 +0200

    Date

--- src/edu/school/Date.java
+++ src/edu/school/Date.java
@@ -8,13 +8,13 @@ public class Date {
 	int month;
 	int year;
 	public Date(int s, int m, int h, int d, int mon, int y) {
-		if(s>=0 && s<=60) {
+		if(s>=0 && s<=59) {
 			sec = s;
 		}
-		if(m>=0 && m<=60) {
+		if(m>=0 && m<=59) {
 			min = m;
 		}
-		if(h>=1 && h<=24) {
+		if(h>=0 && h<=23) {
 			hour = h;
 		}
 		if(d>=1 && d<=31) {
@@ -33,7 +33,7 @@ public class Date {
 		day = 1;
 		month = 1;
 		year = 1;
-	}// Konstruktorut e za dannite da priemat takiva stoinosti purvonachalno i kogao usloviqta v predniq ne sa izpulneni
+	}// Konstruktorut e za dannite da priemat takiva stoinosti purvonachalno i kogato usloviqta v predniq ne sa izpulneni
 	
 	public int getSec() {
 		return sec;
@@ -86,19 +86,26 @@ public class Date {
 	}
 	
 	public int NumberOfDate(Date d) {
-		for(int i=1; i<=d.month; i++) {
-			if(d.month == 2) {
-				d.day += 28;
-				return d.day;
-			}else if(d.month == 2 && d.LeapYear(d)) {
-				d.day += 29;
-				return d.day;
-			}else {
+		if(d.month == 1) {
+			return d.day;
+		}else if(d.month == 2) {
+			d.day += 31;
+			return d.day;
+		}else if(d.month == 3) {
+			d.day += 59;
+		}else if(d.month == 3 && d.LeapYear(d)) {
+			d.day += 60;
+			return d.day;
+		}else {
+			for(int i=3; i<=d.month; i++) {
+				if(i%2 == 0)
+					d.day += 30;
 				d.day += 31;
-				return d.day;
 			}
-				
+			return d.day;
 		}
+				
+		
 		return d.day;
 		
 			
@@ -106,8 +113,24 @@ public class Date {
 	
 	public void SecIncrease() {
 		sec += 1;
+		if(sec == 60) {
+			min += 1;
+			if(min == 60) {
+				hour += 1;
+				if(hour == 24) {
+					day += 1;
+					if(day == 31) {
+						month += 1;
+						if(month == 12) {
+							year += 1;
+						}
+					}
+				
+				}
+			}
+		}
 	}
-	}
+}
 	
 	
 
--- src/edu/school/Date_Test.java
+++ src/edu/school/Date_Test.java
@@ -4,12 +4,15 @@ public class Date_Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Date test = new Date(21, 12, 10, 21, 2, 2020);
-		Date d = new Date(33, 21, 3, 12, 8, 2010);
-		Date y = new Date(21, 12, 10, 21, 10, 2029);
-		System.out.println(test.compareTo(d));
-		System.out.println(y.LeapYear(y));
-		System.out.println(test.NumberOfDate(test));
+		Date test1 = new Date(21, 12, 10, 21, 5, 2020);
+		Date test2 = new Date(33, 21, 3, 12, 8, 2010);
+		Date test3 = new Date(21, 12, 10, 21, 10, 2029);
+		System.out.println(test1.compareTo(test2));
+		if(test3.LeapYear(test3)) {
+			System.out.println("Godinata e visokosna");
+		}
+		System.out.println("Godinata ne e visokosna");
+		System.out.println(test1.NumberOfDate(test1));
 	}
 
 }
