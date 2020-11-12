87873e309d9c5eef52cca1f81341a2b01839eac7
admin-45053 <admin-45053@admin-45053-PC>
2020-11-12 12:28:34 +0200

    Event GUI

--- src/edu/school/Event_GUI.java
+++ src/edu/school/Event_GUI.java
@@ -19,45 +19,53 @@ public class Event_GUI {
 		window.add(lblMonday);
 		
 		JLabel lblTuesday = new JLabel("Tue");
-		lblTuesday.setBounds(120, 10, 80, 30);
+		lblTuesday.setBounds(100, 10, 80, 30);
 		window.add(lblTuesday);
 		
 		JLabel lblWednesday = new JLabel("Wed");
-		lblWednesday.setBounds(230, 10, 80, 30);
+		lblWednesday.setBounds(190, 10, 80, 30);
 		window.add(lblWednesday);
 		
 		JLabel lblThursday = new JLabel("Thu");
-		lblThursday.setBounds(340, 10, 80, 30);
+		lblThursday.setBounds(280, 10, 80, 30);
 		window.add(lblThursday);
 		
 		JLabel lblFriday = new JLabel("Fri");
-		lblFriday.setBounds(450, 10, 80, 30);
+		lblFriday.setBounds(370, 10, 80, 30);
 		window.add(lblFriday);
 		
 		JLabel lblSaturday = new JLabel("Sat");
-		lblSaturday.setBounds(560, 10, 80, 30);
+		lblSaturday.setBounds(460, 10, 80, 30);
 		window.add(lblSaturday);
 		
 		JLabel lblSunday = new JLabel("Sun");
-		lblSunday.setBounds(670, 10, 80, 30);
+		lblSunday.setBounds(550, 10, 80, 30);
 		window.add(lblSunday);
 		
 		JPanel[] days = new JPanel[31];
+		int lengthX = 0;
+		int lengthY = 0;
+		int counterWeekends = 5;
 		
 		for(int i = 0; i<days.length; i++) {
 			days[i] = new JPanel();
-			int lengthX = 100;
-			int lengthY = 40;
-			days[i].setBounds(lengthX, lengthY, 80, 30);
-			if(lengthX >= 670) {
+			if(i%7 == 0) {
 				lengthX = 0;
 				lengthY += 50;
 			}
-			if(lengthX < 560) {
-				days[i].setBackground(Color.gray);
+			else {
+				lengthX += 90;
+			}
+			days[i].setBounds(lengthX, lengthY, 80, 30);
+			if(i == counterWeekends) {
+				days[i].setBackground(Color.red);
+				counterWeekends += 1;
+				if(counterWeekends%7 == 0) {
+					counterWeekends += 5;
+				}
 			}
 			else {
-				days[i].setBackground(Color.red);
+				days[i].setBackground(Color.gray);
 			}
 			window.add(days[i]);
 		}
